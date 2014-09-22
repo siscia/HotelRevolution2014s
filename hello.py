e709690cf1a2a17d93551b290f4ba0cd2b8ec848
siscia <simone@mweb.biz>
2014-09-22 14:04:55 +0200

    dump

---
+++ hello.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from jinja2 import Environment, PackageLoader
+from a import a
+
+app = Flask(__name__)
+
+env = Environment(loader=PackageLoader('hello', 'templates'))
+
+@app.route("/<nome>")
+def hello(nome):
+    mappa = {"titolo" : nome}
+    template = env.get_template("test.html")
+    return template.render(titolo=nome, paragrafo="ciao")
+
+@app.route("/a")
+def route_a():
+    return """<html>
+    <body>
+     <h1> Titolo </h1> 
+     <p>paragrafo</p>
+    </body>
+    </html>"""
+
+if __name__ == "__main__":
+    app.run()
